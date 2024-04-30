962bb77bf0467005b16d27a575cad4484cc4d1a6
adewale <adewalekujore6@gmail.com>
2024-04-30 22:00:36 +0100

    init commit

---
+++ src/com/craftinginterpreters/lox/Rpn.java
@@ -0,0 +1,59 @@
+package com.craftinginterpreters.lox;
+
+public class Rpn implements Expr.Visitor<String>{
+
+    public String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    @Override
+    public String visitBinaryExpr(Expr.Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Expr.Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Expr.Literal expr) {
+        if (expr.value == null) return "nil";
+        return expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Expr.Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < exprs.length; i++) {
+            builder.append(exprs[i].accept(this));
+            if (i != exprs.length - 1)
+                builder.append(" ");
+        }
+        if (!name.equals("group")) {
+            builder.append(" ");
+            builder.append(name);
+        }
+
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Expr expression = new Expr.Binary(
+                new Expr.Grouping(new Expr.Binary(new Expr.Literal(1), new Token(TokenType.PLUS, "+", null, 1), new Expr.Literal(2))
+                ), new Token(TokenType.STAR, "*", null, 1),
+                new Expr.Grouping(
+                        new Expr.Binary(new Expr.Literal(4),
+                                new Token(TokenType.MINUS, "-", null, 1),
+                                new Expr.Literal(3))
+                )
+        );
+
+        System.out.println(new Rpn().print(expression));
+    }
+}
